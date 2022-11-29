0676431d175df3b40541070c65107d042c3e70c9
pqhtobi <hung.phan_dp04@devplus.edu.vn>
2022-11-29 15:31:07 +0700

    CRUD role/#6 role

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { DpRoleController } from "../controllers/Role.controllers.js";
 
 const router = express.Router();
 
+router.get("/role", DpRoleController.get);
+router.post("/role", DpRoleController.create);
+router.patch("/role/:id", DpRoleController.update);
+router.delete("/role/:id", DpRoleController.delete);
 
 
 export default router;
---
+++ src/services/index,.js
@@ -0,0 +1,1 @@
+export {DpRoleSchema} from "./Role.services.js"
