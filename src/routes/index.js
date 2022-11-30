36dab52fef1bd2d1f8136168f16ae05c96d9445d
ThanhTuanDp04 <115678046+ThanhTuanDp04@users.noreply.github.com>
2022-11-30 15:22:53 +0700

    fix create and verify tuan/#2 login

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,8 @@ import express from "express";
 import { UserController } from "../controllers/User.controllers.js";
 import { upload } from "../middlewares/MulterMiddleWare.js";
 import { DpRoleController } from "../controllers/Role.controllers.js";
-import { authController } from "../controllers/authController.js";
-import { middlewareController } from "../middlewares/middlewareController.js";
 
 const router = express.Router();
-
-router.post("/auth/register", authController.registerUser);
-router.post("/auth/login", authController.loginUser);
-router.post("/auth/logout",middlewareController.verifyToken, authController.userLogout);
 
 router.get("/user", UserController.get);
 router.post("/user", upload.single("Avatar"), UserController.create);
@@ -20,5 +14,9 @@ router.get("/role", DpRoleController.get);
 router.post("/role", DpRoleController.create);
 router.patch("/role/:id", DpRoleController.update);
 router.delete("/role/:id", DpRoleController.delete);
+
+router.post("/auth/register", authController.registerUser);
+router.post("/auth/login", authController.loginUser);
+router.post("/auth/logout",middlewareController.verifyToken, authController.userLogout);
 
 export default router;
