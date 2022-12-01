fa7728564fe72b82420c846716666c3dff071c9b
ThanhTuanDp04 <115678046+ThanhTuanDp04@users.noreply.github.com>
2022-12-01 13:38:36 +0700

    fixbug file change password tuan/#4 change password

--- src/controllers/Auth.controller.js
+++ src/controllers/Auth.controller.js
@@ -84,7 +84,6 @@ export const authController = {
               Name : user.Name,
               Avatar : user.Avatar,
               RoleID : user.RoleID,
-              Password : user.Password
             },
           },
           success: true,
